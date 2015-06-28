63bfa102bdeb44074b842099025623c7d8d3b3f8
Johan Jatko <johan.jatko@outlook.com>
2015-06-28 23:47:51 +0200

    Yes what

--- ptOS/App_Start/BundleConfig.cs
+++ ptOS/App_Start/BundleConfig.cs
@@ -11,7 +11,8 @@ namespace ptOS
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/jquery.*",
-                        "~/Scripts/jquery-*"));
+                        "~/Scripts/jquery-*",
+                        "~/Scripts/jVectorMaps/*.js"));
 
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
